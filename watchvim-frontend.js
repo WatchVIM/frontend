d67c2d5ca0508ec0d810c63c0bd5a8e777944165
WatchVIM <support@watchvim.com>
2025-11-20 12:42:23 -0700

    watchvim-frontend.js

--- watchvim-frontend.js
+++ watchvim-frontend.js
@@ -1,1 +1,2 @@
-WATCHVIM_CONFIG.MANIFEST_URL = https://hlumlzbtilvrhwxa.public.blob.vercel-storage.com;
+WATCHVIM_CONFIG.MANIFEST_URL = https://hlumlzbtilvrhwxa.public.blob.vercel-storage.com/catalog-1763667471036-iFR6dFVxU2De2ue2lHEAeVR66Kql6f.json
+;
